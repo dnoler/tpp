9065a3b2abe1a29c5466d72ed40bd185cbc57f5f
Danny Noler <Danny.Noler@availity.com>
2018-08-13 22:05:53 -0400

    Add logger

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'debug',
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple()
+    })
+  ]
+});
+
+logger.info('Starting up TPP Server');
 
 const app = express();
+
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
@@ -8,3 +22,5 @@ app.get('/', function (req, res) {
 });
 
 app.listen(3000);
+
+logger.info('Up and running on port 3000');
