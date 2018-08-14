ba5b2db3f93ae0898b0981bb9e7575b1b7149355
Danny Noler <Danny.Noler@availity.com>
2018-08-13 22:31:51 -0400

    Play with express routes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const winston = require('winston');
+const uuid = require('uuid');
 
 const logger = winston.createLogger({
   level: 'debug',
@@ -19,6 +20,25 @@ const app = express();
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.send('hello world');
+});
+
+const gameMap = {};
+
+app.post('/newGame', (req, res) => {
+  logger.info('New game request received');
+  const gameId = uuid.v4();
+  gameMap[gameId] = 'active';
+  res.send({
+    status: 'success',
+    gameId
+  });
+});
+
+app.post('/gameAction/:gameId', (req, res) => {
+  const gameId = req.param('gameId');
+  logger.info(`Game action on ${gameId}`);
+  const gameStatus = gameMap[gameId] || 'null';
+  res.send(`Game status for ${gameId} is ${gameStatus}`);
 });
 
 app.listen(3000);
